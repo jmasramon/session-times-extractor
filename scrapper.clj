ac1049b11dac7c70cf237428617401fb50519e90
Jordi Masramon <jordi.masramon@gmail.com>
2021-10-26 19:08:02 +0200

    finished the converting functions

--- scrapper.clj
+++ scrapper.clj
@@ -1,11 +1,12 @@
-(require
- '[clojure.java.io :as io]
- '[clojure.string :as str]
- '[clj-time.coerce :as c]
- '[clj-time.format :as f])
+(ns  scrapper
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [clj-time.coerce :as c]
+   [clj-time.format :as f]
+   [clojure.java.shell :refer [sh]]))
 
-
-(defn readFile [file]
+(defn convertFile [file]
   (with-open [rdr (io/reader file)]
     (doseq [line (line-seq rdr)]
       (let [splitted (str/split (str line) #": ")
@@ -19,26 +20,55 @@
                            (c/from-long p-time))]
                 (println
                  key
-                 value)))))))))
+                 value)
+                (spit (str "./human-" (subs file 2)) (str key " " value "\n") :append true)))))))))
 
-(defn returnfilelist
+(defn return-only-times-file-list
   "returns all the files changed today in a directory"
   [d]
   (let [dir (io/file d)
         files (for [file (.listFiles dir)
                     :when (and (not (.isDirectory file))
+                               (str/includes? (str file) "times-only")
                                (str/includes? (str file) ".json"))]
                 (str file))]
     (doseq [f files] (println f)) ;; leaving this in for debugging
     files))
 
-(seq (.list (clojure.java.io/file ".")))
+(defn return-only-session-file-list
+  "returns all the files changed today in a directory"
+  [d]
+  (let [dir (io/file d)
+        files (for [file (.listFiles dir)
+                    :when (and (not (.isDirectory file))
+                               (str/includes? (str file) "session")
+                               (str/includes? (str file) ".json"))]
+                (str file))]
+    (doseq [f files] (println f)) ;; leaving this in for debugging
+    files))
+;; easier version to find all files 
+;;(filter #(str/includes? % ".json") (seq (.list (clojure.java.io/file "."))))
+
+(defn convert-all-files
+  [d]
+  (let [files (return-only-times-file-list d)]
+    (doseq [file files]
+      (println "converting file: " file)
+      (convertFile file))))
+
+(defn output-file
+  [file]
+  (str "./times-only" (subs file 9)))
+
+(defn json-to-flatten-times
+  [d]
+  (let [files (return-only-session-file-list d)]
+    (doseq [file files]
+      (let [new-file (output-file file)]
+        (println "converting file: " file "to: " new-file)
+        (sh "./time-extractor.sh" (str file) new-file)))))
 
 (defn -main [& args]
-  (println "files found:")
-  (returnfilelist ".")
-  (println "first session:")
-  (readFile "only-times.json")
-  (println "second session:")
-  (readFile "only-times-3.json"))
+  (json-to-flatten-times ".")
+  (convert-all-files "."))
 
--- time-extractor.sh
+++ time-extractor.sh
@@ -1,1 +1,3 @@
-cat  session-3.json | jq '[leaf_paths as $path | {"key": $path | join("."), "value": getpath($path)}]  | from_entries' |  jq 'with_entries(if (.key|test(".(start|end|client)Time.*")) then ( {key: .key, value: .value } ) else empty end )' > only-times-3.json
+#bin/bash
+
+cat   $1 | jq '[leaf_paths as $path | {"key": $path | join("."), "value": getpath($path)}]  | from_entries' |  jq 'with_entries(if (.key|test(".(start|end|client)Time.*")) then ( {key: .key, value: .value } ) else empty end )' > $2
