ad4f7c4f9881ceef5dd08be3b0b06e969c1aa8ba
Jordi Masramon <jordi.masramon@gmail.com>
2021-10-27 20:11:36 +0200

    corrected a flaw in the script

--- time-extractor.sh
+++ time-extractor.sh
@@ -1,2 +1,2 @@
 #bin/bash
-cat   $1 | jq '[leaf_paths as $path | {"key": $path | join("."), "value": getpath($path)}]  | from_entries' |  jq 'with_entries(if (.key|test(".(start|end|client)Time.*")) then ( {key: .key, value: .value } ) else empty end )' > $2
+cat   $1 | jq '[leaf_paths as $path | {"key": $path | join("."), "value": getpath($path)}]  | from_entries' |  jq 'with_entries(if (.key|test("(start|end|client)Time.*")) then ( {key: .key, value: .value } ) else empty end )' > $2
